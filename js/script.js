c21ef10a336db3133cffe18f86dedd75f2bbd679
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2024-04-15 10:12:33 -0400

    The document has now some rudimentary responsiveness and the navbar behaves as expected. Should now focus on fixing the layout of the rest of the elements and populating the final images

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function () {
   var navbar = document.getElementById('navbar');
   var scrollPosition = window.scrollY;
 
-  if (scrollPosition > 10) {
+  if (scrollPosition > 20) {
     navbar.classList.add('fixed');
   } else {
     navbar.classList.remove('fixed');
